b44f62f2221e1471ef91f5e360e257f014c1ae60
niels-dn <nielsdn@dadlnet.dk>
2026-06-02 07:17:02 +0200

    Add shared footer.js

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+(function(){
+  const year = new Date().getFullYear();
+  const footer = document.createElement('footer');
+  footer.id = 'skin-footer';
+  footer.innerHTML = `
+    <span>&copy; ${year} Skinnervation</span>
+    <span>
+      Version 1.1
+      &nbsp;&middot;&nbsp;
+      <a href="about.html">Legal disclaimer</a>
+      &nbsp;&middot;&nbsp;
+      <a href="https://buymeacoffee.com/skinnervation" target="_blank" rel="noopener">&#9749; Buy us a coffee</a>
+    </span>
+  `;
+  document.body.appendChild(footer);
+})();
