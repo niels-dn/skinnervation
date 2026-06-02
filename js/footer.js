6ef687d624000dc86c7d3a26b4d7afcc9b11428d
niels-dn <nielsdn@dadlnet.dk>
2026-06-02 20:17:11 +0200

    Add short copyright for mobile

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,8 @@
   const footer = document.createElement('footer');
   footer.id = 'skin-footer';
   footer.innerHTML = `
-    <span>Skinnervation &copy; 2026 &nbsp;&middot;&nbsp; Niels Dalsgaard Nielsen &nbsp;&middot;&nbsp; All rights reserved</span>
+    <span class="footer-copy-full">Skinnervation &copy; 2026 &nbsp;&middot;&nbsp; Niels Dalsgaard Nielsen &nbsp;&middot;&nbsp; All rights reserved</span>
+    <span class="footer-copy-short">Skinnervation &copy; 2026 &nbsp;&middot;&nbsp; Niels D. Nielsen</span>
     <span>
       Version 1.1
       &nbsp;&middot;&nbsp;
